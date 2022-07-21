78dc748e97c010af87f53ad0d193769f1b846bb8
Nicolas Rojas <nico_rojas09@hotmail.com>
2022-07-21 11:15:11 -0300

    clase2

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let usuario = prompt("ingrese su nombre");
+let resultado = prompt("resuelva 5 + 2");
+let resultado2 = prompt("resuelva 5 + 5");
+if(((resultado == 7)&&(resultado2 == 10))||(usuario == "Hawking")){
+    console.log("Perfecto eres un genio");
+}
+else{
+    console.warn("No me parece que este chico sea muy listo - Gallo Claudio");
+}
