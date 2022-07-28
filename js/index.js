e33a96725458c5dc4a2bfaad3986188f0ce6d7ce
Nicolas Rojas <nico_rojas09@hotmail.com>
2022-07-28 12:15:04 -0300

    desafioOptativo

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-let usuario = prompt("ingrese su nombre");
-let resultado = prompt("resuelva 5 + 2");
-let resultado2 = prompt("resuelva 5 + 5");
-if(((resultado == 7)&&(resultado2 == 10))||(usuario == "Hawking")){
-    console.log("Perfecto eres un genio");
-}
-else{
-    console.warn("No me parece que este chico sea muy listo - Gallo Claudio");
+debugger
+let errado = true
+let iniciar = confirm("desea iniciar el test?")
+if(iniciar){
+    let usuario = prompt("ingrese su nombre")
+    do{
+        let resultado = prompt("resuelva 5 + 2")
+        let resultado2 = prompt("resuelva 5 + 5")
+        if(((resultado == 7)&&(resultado2 == 10))||(usuario == "Hawking")){
+            console.log("Perfecto eres un genio")
+            alert("Perfecto eres un genio")
+            errado = false
+        }
+        else{
+            console.warn("intentalo de nuevo")
+            alert("intentalo de nuevo")
+            errado = true
+        }
+    }while(errado)
 }
